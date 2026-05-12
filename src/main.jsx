d10e8123eae96e3f474f8a7b854ec7fda7c5ba9d
Veloweb <velowebfz@proton.me>
2026-05-12 21:49:44 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import './i18n/translations' // init
+import './i18n/translations'   // инициализация i18n
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
