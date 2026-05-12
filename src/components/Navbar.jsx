5dc5f0190e23987290c9ca7f228747264c4e1cab
Veloweb <velowebfz@proton.me>
2026-05-12 22:09:40 +0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useTranslation } from 'react-i18next'
 import { useState } from 'react'
-import './navbar.css'
+import './navbar.css'        // ← точное имя в lowercase
 
 export default function Navbar() {
   const { i18n } = useTranslation()
