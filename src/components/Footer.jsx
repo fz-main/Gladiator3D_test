2c9b155f11f1015ef383ee1eebf5529dd9396971
Veloweb <velowebfz@proton.me>
2026-05-12 22:19:34 +0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer style={{ padding: '5vh 5vw', textAlign: 'center', opacity: 0.6, pointerEvents: 'auto' }}>
-      <p>© 2026 Gladiator Tattoo Studio. All rights reserved. Rybná 650/1, Prague 1.</p>
+      <p>© 2026 Gladiator Tattoo Studio. Rybná 650/1, Prague 1.</p>
     </footer>
   )
 }
